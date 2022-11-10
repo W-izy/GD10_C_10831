b90eef6bda1d0e1766e27d0e1d6c535f1c77fc5d
wisnawa <putuwisnawaandrian@yahoo.co.id>
2022-11-10 15:44:56 +0700

    Android Studio

---
+++ app/src/main/java/com/wisnawa/gd10_c_10831/RClient.kt
@@ -0,0 +1,15 @@
+package com.wisnawa.gd10_c_10831
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RClient {
+    private const val BASE_URL = "http://10.53.6.230/ci4-apiserver/public/"
+    val instances:api by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(api::class.java)
+    }
+}
